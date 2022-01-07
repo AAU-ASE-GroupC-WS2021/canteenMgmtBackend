a60b3cffec16132a7e843570ea40407761e74849
Elias <elias.santner@edu.htl-villach.at>
2022-01-07 20:22:25 +0100

    Replaced concrete implementation with interface

--- src/test/java/edu/aau/groupc/canteenbackend/services/DishServiceTest.java
+++ src/test/java/edu/aau/groupc/canteenbackend/services/DishServiceTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DishServiceTest {
 
     @Autowired
-    private DishService dishService;
+    private IDishService dishService;
 
     @Test
     public void testAddDish() {
