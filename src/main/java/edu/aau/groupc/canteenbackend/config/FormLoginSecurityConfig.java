9b41ca2ba240151604c0673a8d8d6dcfe6edd8fe
Elias <elias.santner@edu.htl-villach.at>
2022-01-17 23:16:30 +0100

    Configured CSRF tokens

--- src/main/java/edu/aau/groupc/canteenbackend/config/FormLoginSecurityConfig.java
+++ src/main/java/edu/aau/groupc/canteenbackend/config/FormLoginSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * Configuration to allow all HTTPS requests without authentication
@@ -18,6 +19,7 @@ public class FormLoginSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/**")
                 .permitAll();
-        http.csrf().disable();
+        // set CSRF token as cookie
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 }
