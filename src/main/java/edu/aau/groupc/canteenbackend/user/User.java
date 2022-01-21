ad3fb4b1a5d5af8aef07fdeb6f4472f136f7b231
emgo_xxq <31079238+emirgoran@users.noreply.github.com>
2022-01-21 21:08:22 +0100

    Create User.java

---
+++ src/main/java/edu/aau/groupc/canteenbackend/user/User.java
@@ -0,0 +1,20 @@
+package edu.aau.groupc.canteenbackend.user;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"username"}),
+        @UniqueConstraint(columnNames = {"email"})
+})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+
+}
