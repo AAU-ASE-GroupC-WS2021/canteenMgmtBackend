1659f73f05bd25689cbad0849a244e8512eec53f
emgo_xxq <31079238+emirgoran@users.noreply.github.com>
2022-01-21 21:21:02 +0100

    Create RoleRepository.java

---
+++ src/main/java/edu/aau/groupc/canteenbackend/user/RoleRepository.java
@@ -0,0 +1,11 @@
+package edu.aau.groupc.canteenbackend.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+
+}
