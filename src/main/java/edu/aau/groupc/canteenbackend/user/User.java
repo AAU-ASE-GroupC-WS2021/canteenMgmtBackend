c2705284567eee656279b38542c46c2ddd776ba7
emgo_xxq <31079238+emirgoran@users.noreply.github.com>
2022-01-21 22:30:15 +0100

    Remove Role

--- src/main/java/edu/aau/groupc/canteenbackend/user/User.java
+++ src/main/java/edu/aau/groupc/canteenbackend/user/User.java
@@ -18,10 +18,4 @@ public class User {
     private String username;
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
-    private Set<Role> roles;
-
 }
