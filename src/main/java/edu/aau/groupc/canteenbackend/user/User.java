f99d19a81968253d3bfe113701d86f0ce753999b
emgo_xxq <31079238+emirgoran@users.noreply.github.com>
2022-01-21 21:30:46 +0100

    Remove email and name fields from User
    To simplify implementation

--- src/main/java/edu/aau/groupc/canteenbackend/user/User.java
+++ src/main/java/edu/aau/groupc/canteenbackend/user/User.java
@@ -9,16 +9,13 @@ import java.util.Set;
 @Entity
 @Table(name = "users", uniqueConstraints = {
         @UniqueConstraint(columnNames = {"username"}),
-        @UniqueConstraint(columnNames = {"email"})
 })
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String name;
     private String username;
-    private String email;
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
