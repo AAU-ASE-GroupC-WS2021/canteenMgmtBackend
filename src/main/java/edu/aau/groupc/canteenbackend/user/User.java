99ba282f7f4bd2c49d715fa9f4c4b3082310a316
emgo_xxq <31079238+emirgoran@users.noreply.github.com>
2022-01-21 21:20:57 +0100

    Update User.java

--- src/main/java/edu/aau/groupc/canteenbackend/user/User.java
+++ src/main/java/edu/aau/groupc/canteenbackend/user/User.java
@@ -1,7 +1,11 @@
 package edu.aau.groupc.canteenbackend.user;
 
-import javax.persistence.*;
+import lombok.Data;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
 @Entity
 @Table(name = "users", uniqueConstraints = {
         @UniqueConstraint(columnNames = {"username"}),
@@ -16,5 +20,11 @@ public class User {
     private String username;
     private String email;
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles;
 
 }
