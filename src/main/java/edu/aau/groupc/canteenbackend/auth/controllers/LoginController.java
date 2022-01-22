b1898bb83a9b9377a5cab328d3819486d051b38b
emgo_xxq <31079238+emirgoran@users.noreply.github.com>
2022-01-22 14:38:14 +0100

    Add "Auth-Token" header when logged in

--- src/main/java/edu/aau/groupc/canteenbackend/auth/controllers/LoginController.java
+++ src/main/java/edu/aau/groupc/canteenbackend/auth/controllers/LoginController.java
@@ -5,11 +5,16 @@ import edu.aau.groupc.canteenbackend.auth.dto.LoginDto;
 import edu.aau.groupc.canteenbackend.auth.dto.LogoutDto;
 import edu.aau.groupc.canteenbackend.auth.services.IAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class LoginController {
@@ -21,13 +26,21 @@ public class LoginController {
         this.authService = authService;
     }
 
+    // If successfully logged in, an "Auth-Token" header will be sent back.
     @PostMapping(value = "/api/auth")
     public ResponseEntity<?> login(@RequestBody LoginDto newAuth) {
-        if (this.authService.login(newAuth.getUsername(), newAuth.getPassword()) == null) {
-            return new ResponseEntity<>("Incorrect login credentials!", HttpStatus.BAD_REQUEST);
+        Auth auth;
+
+        if ((auth = this.authService.login(newAuth.getUsername(), newAuth.getPassword())) == null) {
+            return new ResponseEntity<>("Incorrect or non-existing login credentials!", HttpStatus.BAD_REQUEST);
         }
 
-        return new ResponseEntity<>("Logged in successfully.", HttpStatus.OK);
+        HttpHeaders headers = new HttpHeaders();
+        List<String> headersList = new ArrayList<>();
+        headersList.add(auth.getToken());
+        headers.put("Auth-Token", headersList);
+
+        return new ResponseEntity<>("Logged in successfully.", headers, HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/api/auth")
