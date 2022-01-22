1ee49f2d834130be43f373dc73458aa977ab2905
emgo_xxq <31079238+emirgoran@users.noreply.github.com>
2022-01-22 14:07:08 +0100

    Delete unused methods

--- src/main/java/edu/aau/groupc/canteenbackend/user/repositories/IUserRepository.java
+++ src/main/java/edu/aau/groupc/canteenbackend/user/repositories/IUserRepository.java
@@ -7,8 +7,6 @@ import java.util.Optional;
 
 public interface IUserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsername(String username);
-
     Boolean existsByUsername(String username);
 
 }
