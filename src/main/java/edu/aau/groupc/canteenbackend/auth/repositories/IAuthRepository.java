050cd0ba8a63ab59007a92e46730392bd8f62b7f
emgo_xxq <31079238+emirgoran@users.noreply.github.com>
2022-01-22 14:06:54 +0100

    Delete unused methods

--- src/main/java/edu/aau/groupc/canteenbackend/auth/repositories/IAuthRepository.java
+++ src/main/java/edu/aau/groupc/canteenbackend/auth/repositories/IAuthRepository.java
@@ -8,14 +8,6 @@ import java.util.Optional;
 
 public interface IAuthRepository extends JpaRepository<Auth, Long> {
 
-    Optional<Auth> findByUsername(String username);
-
-    Optional<Auth> findByUsernameAndToken(String username, String token);
-
-    Boolean existsByUsername(String username);
-
-    Boolean deleteByUsername(String username);
-
     Long deleteByUsernameAndToken(String username, String token);
 
 }
