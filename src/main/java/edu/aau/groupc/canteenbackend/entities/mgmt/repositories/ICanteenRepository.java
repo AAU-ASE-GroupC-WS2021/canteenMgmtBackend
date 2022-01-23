9592bc131a082dfbce26eb725c4c96b733df9304
Elias <elias.santner@edu.htl-villach.at>
2022-01-23 19:03:03 +0100

    Removed accidental duplicate classes

--- src/main/java/edu/aau/groupc/canteenbackend/entities/mgmt/repositories/ICanteenRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.aau.groupc.canteenbackend.entities.mgmt.repositories;
-
-import edu.aau.groupc.canteenbackend.entities.mgmt.Canteen;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICanteenRepository extends JpaRepository<Canteen, Integer> {
-}
