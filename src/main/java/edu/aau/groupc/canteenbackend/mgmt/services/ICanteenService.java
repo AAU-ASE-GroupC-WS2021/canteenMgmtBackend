d42beb68a37ddda0176270ff6fcbc33df7d21d92
Elias <elias.santner@edu.htl-villach.at>
2022-01-23 19:01:42 +0100

    Reverted accidental package move

---
+++ src/main/java/edu/aau/groupc/canteenbackend/mgmt/services/ICanteenService.java
@@ -0,0 +1,20 @@
+package edu.aau.groupc.canteenbackend.mgmt.services;
+
+import edu.aau.groupc.canteenbackend.mgmt.Canteen;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICanteenService {
+    List<Canteen> findAll();
+    Optional<Canteen> findById(int id);
+    Canteen create(Canteen canteen);
+
+    /**
+     * Update canteen entity.
+     * @param canteen Canteen to update
+     * @return Updated canteen
+     * @throws IllegalArgumentException If no canteen with the given id exists.
+     */
+    Canteen update(Canteen canteen) throws IllegalArgumentException;
+}
