7d11f34dbb183342841758ad15ec3346aa7f8a9e
Elias <elias.santner@edu.htl-villach.at>
2022-01-26 16:47:26 +0100

    Removed unused import

--- src/main/java/edu/aau/groupc/canteenbackend/validation/ExceptionHandler.java
+++ src/main/java/edu/aau/groupc/canteenbackend/validation/ExceptionHandler.java
@@ -13,7 +13,6 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionHandler extends ResponseEntityExceptionHandler {
