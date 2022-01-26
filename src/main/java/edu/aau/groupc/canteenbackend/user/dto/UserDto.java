95011fae3d68a8c11f015a8078f9b92be9137a9f
emgo_xxq <31079238+emirgoran@users.noreply.github.com>
2022-01-27 00:17:51 +0100

    Update UserDto.java

--- src/main/java/edu/aau/groupc/canteenbackend/user/dto/UserDto.java
+++ src/main/java/edu/aau/groupc/canteenbackend/user/dto/UserDto.java
@@ -5,17 +5,20 @@ import edu.aau.groupc.canteenbackend.user.User;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 @Data
 public class UserDto implements DTO, Serializable {
 
+    @NotBlank(message = "Username is required")
     @Length(min = 3, message = "Username must be at least 3 characters long!")
     @Length(max = 24, message = "Username cannot be longer than 24 characters!")
     @Pattern(regexp = "^[A-Za-z0-9]+$", message = "Username can contain only alphanumeric characters!")
     private String username;
 
+    @NotBlank(message = "Password is required")
     @Pattern(regexp = "^[a-fA-F0-9]+$", message = "Password can contain only hexadecimal digits!")
     @Length(min = 64, max = 64, message = "Password must be exactly 64 characters long!")
     private String password;
