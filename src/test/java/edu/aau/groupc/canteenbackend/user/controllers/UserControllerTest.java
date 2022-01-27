a5d0907c8502188c00afb4fb9ba2cac524111e5f
emgo_xxq <31079238+emirgoran@users.noreply.github.com>
2022-01-27 22:21:27 +0100

    Update UserControllerTest.java

--- src/test/java/edu/aau/groupc/canteenbackend/user/controllers/UserControllerTest.java
+++ src/test/java/edu/aau/groupc/canteenbackend/user/controllers/UserControllerTest.java
@@ -1,16 +1,14 @@
 package edu.aau.groupc.canteenbackend.user.controllers;
 
 import edu.aau.groupc.canteenbackend.endpoints.AbstractControllerTest;
-import edu.aau.groupc.canteenbackend.user.User;
 import edu.aau.groupc.canteenbackend.user.services.IUserService;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.HttpClientErrorException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ActiveProfiles("H2Database")
 class UserControllerTest extends AbstractControllerTest {
@@ -19,19 +17,107 @@ class UserControllerTest extends AbstractControllerTest {
     IUserService userService;
 
     @Test
-    void userGET_createNewUserManuallyThenGET_returnsCorrect() throws JSONException {
-        userService.create(new User("TestUser123", "0123456789ABCDEF", User.Type.USER));
+    void userPOST_createNewUserUsernameNull_throwsException() {
+        String body = "{ \"password\": \"" + "a".repeat(64) + "\" }";
 
-        ResponseEntity<String> response = makeGetRequest("/api/register");
+        HttpClientErrorException thrown = assertThrows(
+                HttpClientErrorException.class,
+                () -> { makePostRequest("/api/register", body); },
+                "Expected exception to throw, but it didn't"
+        );
 
-        JSONArray expected = new JSONArray();
-        JSONObject object = new JSONObject();
-        object.put("username", "TestUser123");
-        object.put("password", "0123456789ABCDEF");
-        object.put("type", "USER");
-        expected.put(object);
+        assertTrue(thrown.getMessage().contains("Username is required"));
+    }
 
-        JSONAssert.assertEquals(expected.toString(), response.getBody(), false);
+    @Test
+    void userPOST_createNewUserUsernameTooShort_throwsException() {
+        String body = "{ \"username\": \"AB\", \"password\": \"" + "a".repeat(64) + "\" }";
+
+        HttpClientErrorException thrown = assertThrows(
+                HttpClientErrorException.class,
+                () -> { makePostRequest("/api/register", body); },
+                "Expected exception to throw, but it didn't"
+        );
+
+        assertTrue(thrown.getMessage().contains("Username must be at least 3 characters long!"));
+    }
+
+    @Test
+    void userPOST_createNewUserUsernameTooLong_throwsException() {
+        String body = "{ \"username\": \"" + "A".repeat(25) + "\", \"password\": \"" + "a".repeat(64) + "\" }";
+
+        HttpClientErrorException thrown = assertThrows(
+                HttpClientErrorException.class,
+                () -> { makePostRequest("/api/register", body); },
+                "Expected exception to throw, but it didn't"
+        );
+
+        assertTrue(thrown.getMessage().contains("Username cannot be longer than 24 characters!"));
+    }
+
+    @Test
+    void userPOST_createNewUserUsernameContainsForbiddenCharacter_throwsException() {
+        String body = "{ \"username\": \"ABC$\", \"password\": \"" + "a".repeat(64) + "\" }";
+
+        HttpClientErrorException thrown = assertThrows(
+                HttpClientErrorException.class,
+                () -> { makePostRequest("/api/register", body); },
+                "Expected exception to throw, but it didn't"
+        );
+
+        assertTrue(thrown.getMessage().contains("Username can contain only alphanumeric characters!"));
+    }
+
+    @Test
+    void userPOST_createNewUserPasswordTooShort_throwsException() {
+        String body = "{ \"username\": \"TestUser124\", \"password\": \"" + "a".repeat(63) + "\" }";
+
+        HttpClientErrorException thrown = assertThrows(
+                HttpClientErrorException.class,
+                () -> { makePostRequest("/api/register", body); },
+                "Expected exception to throw, but it didn't"
+        );
+
+        assertTrue(thrown.getMessage().contains("Password must be exactly 64 characters long!"));
+    }
+
+    @Test
+    void userPOST_createNewUserPasswordTooLong_throwsException() {
+        String body = "{ \"username\": \"TestUser124\", \"password\": \"" + "a".repeat(65) + "\" }";
+
+        HttpClientErrorException thrown = assertThrows(
+                HttpClientErrorException.class,
+                () -> { makePostRequest("/api/register", body); },
+                "Expected exception to throw, but it didn't"
+        );
+
+        assertTrue(thrown.getMessage().contains("Password must be exactly 64 characters long!"));
+    }
+
+    @Test
+    void userPOST_createNewUserPasswordNull_throwsException() {
+        String body = "{ \"username\": \"TestUser124\" }";
+
+        HttpClientErrorException thrown = assertThrows(
+                HttpClientErrorException.class,
+                () -> { makePostRequest("/api/register", body); },
+                "Expected exception to throw, but it didn't"
+        );
+
+        assertTrue(thrown.getMessage().contains("Password is required"));
+    }
+
+    @Test
+    void userPOST_createNewUserPasswordContainsInvalidChar_throwsException() {
+        String body = "{ \"username\": \"TestUser124\", \"password\": \"" + "a".repeat(63) + "Z\" }";
+
+        HttpClientErrorException thrown = assertThrows(
+                HttpClientErrorException.class,
+                () -> { makePostRequest("/api/register", body); },
+                "Expected exception to throw, but it didn't"
+        );
+
+        assertTrue(thrown.getMessage().contains("Password can contain only hexadecimal digits!"));
     }
 
 }
