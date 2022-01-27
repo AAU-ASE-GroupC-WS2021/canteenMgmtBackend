7769b6d28d65435d130ab701beca60c31b8b4ca5
emgo_xxq <31079238+emirgoran@users.noreply.github.com>
2022-01-27 22:20:34 +0100

    Update UserTest.java

--- src/test/java/edu/aau/groupc/canteenbackend/user/UserTest.java
+++ src/test/java/edu/aau/groupc/canteenbackend/user/UserTest.java
@@ -2,6 +2,8 @@ package edu.aau.groupc.canteenbackend.user;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
@@ -13,6 +15,59 @@ class UserTest {
         assertEquals("username", a.getUsername());
         assertEquals("password", a.getPassword());
         assertEquals(User.Type.GUEST, a.getType());
+    }
+
+    @Test
+    void createUser_testSetters_returnsOK() {
+        User a = new User("", "", User.Type.GUEST);
+
+        a.setId(5L);
+        a.setUsername("username");
+        a.setPassword(("password"));
+        a.setType(User.Type.USER);
+
+        assertEquals(5L, a.getId());
+        assertEquals("username", a.getUsername());
+        assertEquals("password", a.getPassword());
+        assertEquals(User.Type.USER, a.getType());
+    }
+
+    @Test
+    void createUser_testEquals_returnsOK() {
+        User a = new User("username", "password", User.Type.GUEST);
+        User b = new User("username", "password", User.Type.GUEST);
+
+        assertEquals(a, b);
+    }
+
+    @Test
+    void createUser_testEqualsSame_returnsOK() {
+        User a = new User("username", "password", User.Type.GUEST);
+
+        assertEquals(a, a);
+    }
+
+    @Test
+    void createUser_testNotEquals_returnsOK() {
+        User a = new User("username", "password", User.Type.GUEST);
+        User b = new User("username", "password", User.Type.USER);
+
+        assertNotEquals(a, b);
+    }
+
+    @Test
+    void createUser_testNotEqualsNull_returnsOK() {
+        User a = new User("username", "password", User.Type.GUEST);
+
+        assertNotEquals(a, null);
+    }
+
+    @Test
+    void createUser_testHash_returnsOK() {
+        User a = new User("username", "password", User.Type.GUEST);
+        int expectedHash = Objects.hash(a.getId(), a.getUsername(), a.getPassword(), a.getType());
+
+        assertEquals(expectedHash, a.hashCode());
     }
 
 }
