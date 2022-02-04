d24eae4e186725ecb966796b3d17ff96921bc1e8
emirgoran <31079238+emirgoran@users.noreply.github.com>
2022-02-04 17:58:05 +0100

    Update UserController.java

--- src/main/java/edu/aau/groupc/canteenbackend/user/controllers/UserController.java
+++ src/main/java/edu/aau/groupc/canteenbackend/user/controllers/UserController.java
@@ -1,14 +1,18 @@
 package edu.aau.groupc.canteenbackend.user.controllers;
 
+import edu.aau.groupc.canteenbackend.auth.security.Secured;
+import edu.aau.groupc.canteenbackend.user.User;
 import edu.aau.groupc.canteenbackend.user.dto.UserDto;
 import edu.aau.groupc.canteenbackend.user.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RestController
@@ -30,4 +34,16 @@ public class UserController {
         return new ResponseEntity<>("User registered successfully.", HttpStatus.OK);
     }
 
+    @Secured
+    @GetMapping(value = "/api/register")
+    public ResponseEntity<String> getUser(HttpServletRequest req) {
+
+        User authenticatedUser = (User) req.getAttribute("user");
+
+        if (authenticatedUser == null) {
+            return new ResponseEntity<>("Could not load user profile!", HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(authenticatedUser.getUserSelfInfo(), HttpStatus.OK);
+    }
 }
